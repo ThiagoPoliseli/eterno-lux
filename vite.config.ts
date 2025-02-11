e7579be387c25b553dcdb254bb512af7209240d8
Thiago Poliseli <169108232+ThiagoPoliseli@users.noreply.github.com>
2025-02-11 09:43:01 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/eterno-lux/', // Replace with your repository name
+  base: '/https://github.com/ThiagoPoliseli/eterno-lux/', // Replace with your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
